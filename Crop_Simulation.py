fe93532fe901a41498002c56b0082ac7e1cf261d
MattAnderson16 <30393@longroad.ac.uk>
2014-07-01 13:29:48 +0100

    crop simulation GUI tasks 1 and 2

--- Crop_Simulation.py
+++ Crop_Simulation.py
@@ -3,11 +3,54 @@ import sys
 from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 
+from radio_button_widget_class import * #provides the radio button widget
+from wheat_class import *
+from potato_class import *
+
 class CropWindow(QMainWindow):
     """this class creates a main window to observe the growth of a simulated crop"""
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Crop Simulator")
+        self.create_select_crop_layout()
+
+    def create_select_crop_layout(self):
+        #this is the initial layout of the window - to select the crop type
+
+        self.crop_radio_buttons = RadioButtonWidget("Crop Simulation", "Please select a crop", ("Wheat","Potato"))
+        self.instantiate_button = QPushButton("Create Crop")
+
+        #Create layout to hold the widgets
+        self.initial_layout = QVBoxLayout()
+        self.initial_layout.addWidget(self.crop_radio_buttons)
+        self.initial_layout.addWidget(self.instantiate_button)
+
+        self.select_crop_widget = QWidget()
+        self.select_crop_widget.setLayout(self.initial_layout)
+
+        self.setCentralWidget(self.select_crop_widget)
+
+        #connections
+        self.instantiate_button.clicked.connect(self.instantiate_crop)
+
+    def create_view_crop_layout(self,crop_type):
+        #this is the second layout of the window - view the crop growth
+
+        self.growth_label = QLabel("Growth")
+        self.days_label = QLabel("Days Growing")
+        self.status_label = QLabel("Crop Status")
+
+        self.growth_line_edit = QLineEdit()
+        self.days_line_edit = QLineEdit()
+        self.status_line_edit = QLineEdit()
+
+    def instantiate_crop(self):
+        crop_type = self.crop_radio_buttons.selected_button() #get the radio button that was selected
+        if crop_type == 1:
+            self.simulated_crop = Wheat()
+        elif crop_type == 2:
+            self.simulated_crop = Potato()
+        print(self.simulated_crop)
 
 if __name__ == "__main__":
     crop_simulation = QApplication(sys.argv)
